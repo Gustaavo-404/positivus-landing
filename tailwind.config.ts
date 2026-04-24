0de1123f7c9febbd177fb8646c33b3deaba4930d
Gustavo <gustmb2005@gmail.com>
2026-04-24 09:08:57 -0300

    chore: setup inicial do projeto (Next + Tailwind)

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+export default {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#B9FF66',
+                dark: '#191A23',
+                light: '#F3F3F3',
+            }
+        }
+    }
+}
